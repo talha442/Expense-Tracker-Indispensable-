b3476cec193a06f045b912aa139d226b90f49850
talha faisal <57016608+talha442@users.noreply.github.com>
2023-04-09 00:37:20 +0500

    Created Backend Folder Structure & Integrated MondoDB Atlas & created Server

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./db/db");
+const { readdirSync } = require("fs");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+/* middlewares */
+app.use(express.json());
+app.use(cors());
+
+/* routes */
+// Read All Files in routes folder
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
+
+const server = () => {
+  db();
+  app.listen(PORT, () => {
+    console.log("Listening to Port:", PORT);
+  });
+};
+
+server();
