915936e50192a570e5e71010af0ca7f437960fdc
talha faisal <57016608+talha442@users.noreply.github.com>
2023-04-09 16:59:55 +0500

    Created APIs (POST, GET, DELETE)

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,3 +1,52 @@
+const IncomeSchema = require("../models/incomeModel");
+
+/* ADD INCOME */
 exports.addIncome = async (req, res) => {
-  console.log(req.body);
+  // destructuring
+  const { title, amount, category, description, date } = req.body;
+  const income = IncomeSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+
+  try {
+    // validation
+    if (!title || !category || !description || !date) {
+      return res.status(400).json({ message: "All field are required" });
+    }
+    if (amount <= 0 || amount === "number") {
+      return res.status(400).json({ message: "Amount must be a number" });
+    }
+    await income.save();
+    res.status(200).json({ message: "Income added" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+
+  console.log(income);
+};
+
+/* GET INCOME */
+exports.getIncomes = async (req, res) => {
+  try {
+    const incomes = await IncomeSchema.find().sort({ createdAt: -1 });
+    res.status(200).json(incomes);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+/* DELETE INCOME */
+exports.deleteIncome = async (req, res) => {
+  const { id } = req.params;
+  IncomeSchema.findByIdAndDelete(id)
+    .then((income) => {
+      res.status(200).json({ message: "Income deleted successfully" });
+    })
+    .catch((error) => {
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
 };
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,14 @@
-const { addIncome } = require('../controllers/income');
-const router = require('express').Router();
+const {
+  addIncome,
+  deleteIncome,
+  getIncomes,
+} = require("../controllers/income");
+const router = require("express").Router();
 
 // Created Endpoint for Income
-router.post('/add-income', addIncome);
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome);
 
 module.exports = router;
