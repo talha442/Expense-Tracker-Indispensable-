e64b5108f26a623f0981b671f2846553a8dd6d2b
talha faisal <57016608+talha442@users.noreply.github.com>
2023-04-10 17:11:40 +0500

    Created Expense APIs & Endpoints

---
+++ backend/controllers/expense.js
@@ -0,0 +1,52 @@
+const ExpenseSchema = require("../models/expenseModel");
+
+/* ADD EXPENSE */
+exports.addExpense = async (req, res) => {
+  // destructuring
+  const { title, amount, category, description, date } = req.body;
+  const expense = ExpenseSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+
+  try {
+    // validation
+    if (!title || !category || !description || !date) {
+      return res.status(400).json({ message: "All field are required" });
+    }
+    if (amount <= 0 || amount === "number") {
+      return res.status(400).json({ message: "Amount must be a number" });
+    }
+    await expense.save();
+    res.status(200).json({ message: "Expense added" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+
+  console.log(expense);
+};
+
+/* GET EXPENSE */
+exports.getExpense = async (req, res) => {
+  try {
+    const expenses = await ExpenseSchema.find().sort({ createdAt: -1 });
+    res.status(200).json(expenses);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+/* DELETE EXPENSE */
+exports.deleteExpense = async (req, res) => {
+  const { id } = req.params;
+  ExpenseSchema.findByIdAndDelete(id)
+    .then((expense) => {
+      res.status(200).json({ message: "Expense deleted successfully" });
+    })
+    .catch((error) => {
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
+};
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,9 @@
 const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expense");
+const {
   addIncome,
   deleteIncome,
   getIncomes,
@@ -9,6 +14,9 @@ const router = require("express").Router();
 router
   .post("/add-income", addIncome)
   .get("/get-incomes", getIncomes)
-  .delete("/delete-income/:id", deleteIncome);
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpense)
+  .delete("/delete-expense/:id", deleteExpense);
 
 module.exports = router;
