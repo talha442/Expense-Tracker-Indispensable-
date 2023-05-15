6361368af8ef0f61a06bbd8b9444e0eb19b33f11
talha faisal <57016608+talha442@users.noreply.github.com>
2023-05-15 11:54:48 +0500

    style navigation side bar and add avatar image

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -35,6 +35,46 @@ const NavStyled = styled.nav`
   padding: 2rem 1.5rem;
   width: 374px;
   height: 100%;
+  background: rgba(252, 246, 249, 0.78);
+  border: 3px solid white;
+  backdrop-filter: blur(4.5px);
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 2rem;
+  .user-con {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      object-fit: cover;
+      background: #fcf6f9;
+      border: 2px solid #fffff;
+      padding: 0.2rem;
+      bax-shadow: 0px 1px 17px rgba(0, 0, 0, 0.06);
+    }
+    h2 {
+      color: rgba(34, 34, 96, 1);
+    }
+    p {
+      color: rgba(34, 34, 96, 0.6);
+    }
+  }
+
+  .menu-items {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    li {
+      display: grid;
+      grid-template-column: 40px auto;
+    }
+  }
 `;
 
 export default Navigation;
